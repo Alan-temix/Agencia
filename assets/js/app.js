b3111016c6655213afcf75e37d1c957a3a909a1a
Alan Temix <Temix@protonmail.com>
2021-07-24 23:51:07 -0500

    'repeat elements bug' fixed

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,9 +31,13 @@ let BUTTONSTATUS = 0;
 const printCars = ((dataCars) =>{
     const tbody_content = document.getElementById('tbody-cars');
     let div_cardDeck = document.querySelector('div.card-deck');
+    let div_carrousel = document.querySelector('div.carousel-inner');
+    let index = 0;
     div_cardDeck.innerHTML = '';
     tbody_content.innerHTML = '';
+    div_carrousel.innerHTML = '';
     dataCars.forEach((car)=>{
+        let status = '';
         const car_HTML = `<tr>
                             <td><a href="#${car.id}"><img src="${car.photo}" alt="" style="max-width: 100%"></a></td>
                             <td>${car.model}</td>
@@ -55,8 +59,14 @@ const printCars = ((dataCars) =>{
             </div>
         </div>
         `
+        index === 0 ? status = ' active' : status = '', index++;
+        const carrousel_html = `
+        <div class="carousel-item ${status}"><img class="d-block w-100" src="${car.photo}" alt="${index} slide"></div>
+        `
         tbody_content.innerHTML += car_HTML;
         div_cardDeck.innerHTML += div_html;
+        div_carrousel.innerHTML += carrousel_html;
+
         // 
         // let htmlFirstDiv = document.createElement('div');
         // let htmlImg = document.createElement('img');
@@ -96,28 +106,6 @@ const printCars = ((dataCars) =>{
         // div_cardDeck.appendChild(htmlFirstDiv)
 
     });
-    for(let i = 0; i < dataCars.length; i++) {
-        let numberImg = i;
-        let imgSrc;
-    
-        dataCars[i]['photo'] === '' ? imgSrc = '' : imgSrc = dataCars[i]['photo'];
-        
-        let carrouselInner = document.querySelector('div.carousel-inner');
-    
-        let imG = document.createElement('img');
-        imG.classList.add('d-block', 'w-100');
-        imG.src = `${imgSrc}`;
-        imG.setAttribute('alt', `${numberImg + 1} slide`);
-    
-        let carrouselItem = document.createElement('div');
-        carrouselItem.classList.add("carousel-item");
-        if(i === 0) {
-            carrouselItem.classList.add("active")
-        }
-    
-        carrouselItem.appendChild(imG);
-        carrouselInner.appendChild(carrouselItem);
-    }
 });
 // printCars(JSON.parse(localStorage.getItem("cars")));
 printCars(cars);
